d09494733230505e1d664dec85deed5ebf29d54e
JColocho <Jcolocho2003@gmail.com>
2025-11-07 11:31:30 -0600

    feat: Controladores para ejercicio3 agregados

---
+++ app/Http/Controllers/TipoClienteController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tipo_cliente;
+use Illuminate\Http\Request;
+
+class TipoClienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tipo_cliente $tipo_cliente)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tipo_cliente $tipo_cliente)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Tipo_cliente $tipo_cliente)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tipo_cliente $tipo_cliente)
+    {
+        //
+    }
+}
